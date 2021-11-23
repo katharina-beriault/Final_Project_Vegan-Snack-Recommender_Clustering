ceee2fd9e1062013bda78c2b5f773ede180612c8
Katharina Beriault <katharina@beriault.de>
2021-11-23 20:13:08 +0100

    update

---
+++ Python/main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+st.write("""# Vegan Snack Recommender""")
+
+st.image("https://latestvegannews.com/wp-content/uploads/2019/07/91FFqqXvBhL._SL1500_.jpg", width=400)
