936d6ce1c93af6af77f8b98cb86fd1d8aa7649d0
Katharina Beriault <katharina@beriault.de>
2021-11-26 15:26:49 +0100

    update

--- Python/presentation.py
+++ Python/presentation.py
@@ -28,7 +28,7 @@ st.markdown('#')
 # Loading dataframe
 df = pd.read_csv('df_clusters.csv')
 
-# function for a random sample of products
+# function for a random sample of products (for first dataframe)
 def random_sample(df):
 
     cl_0 = df[df['cluster_2'] == 0].sample(1)
@@ -40,7 +40,7 @@ def random_sample(df):
 
     return new_df
 
-# function to get photo link of products
+# function to get photo link of products (of first dataframe)
 def get_details(df, pos):
 
     new_df = random_sample(df)
@@ -49,7 +49,7 @@ def get_details(df, pos):
     name = new_df['product'].values[pos]
 
     return link, name
-
+    
 # getting details
 cl_0 = get_details(df, 0)
 cl_1 = get_details(df, 1)
@@ -68,97 +68,122 @@ cl_1_name = cl_1[1]
 cl_2_name = cl_2[1]
 cl_3_name = cl_3[1]
 
-# adjusted function to get product details for final data frame
-def get_details_adj(df, pos):
     
-    name = df['product'].values[pos]
-    link = df['product_link'].values[pos]
-    photo = df['photo_link'].values[pos]
-    
-    return name, link, photo
+# showing four different products 
+st.markdown('##')
+st.text("Please select the snack that appears to be the most tasty to you.")
+#st.text("If you don't like these snack options, just click start again.")
 
-# final recommender function
-def product_recommender(df, product):
+# create grid for photos
+col1, col2 = st.columns(2)
+
+col1.header("Product no. 1")
+col1.write(cl_0_name)
+col1.image(cl_0_link)
+
+col2.header("Product no. 2")
+col2.write(cl_1_name)
+col2.image(cl_1_link)
+
+# create grid for photos
+col3, col4 = st.columns(2)
+
+col3.header("Product no. 3")
+col3.write(cl_2_name)
+col3.image(cl_2_link)
+
+col4.header("Product no. 4")
+col4.write(cl_3_name)
+col4.image(cl_3_link)
+
+
+st.markdown('#')
+st.markdown('#')
+
+# product recommender
+def product_recommender_2(df, option):
     
+    drop_lst = ['Unnamed: 0', 'weight', 'kj', 'kcal', 'fat', 'sat_fat', 'carbs', 'sugar', 'fibre', 'protein', 'salt',
+                    'photo_link', 'price_weight', 'brand_new', 'cluster', 'cluster_2']
     counter = -1
     
     for p in df['product']:
         
         counter +=1
         
-        if p == product:
+        if p == option:
             cluster_product = df['cluster_2'].iloc[counter]
             
             new_snacks = df[df['cluster_2'] == cluster_product].sample(5)
             
-            snack_1 = get_details_adj(new_snacks, 0)
-            snack_2 = get_details_adj(new_snacks, 1)
-            snack_3 = get_details_adj(new_snacks, 2)
-            snack_4 = get_details_adj(new_snacks, 3)
-            snack_5 = get_details_adj(new_snacks, 4)
+            new_snacks = new_snacks.drop(drop_lst, axis=1)
             
         else:
             continue
     
-    return snack_1, snack_2, snack_3, snack_4, snack_5
-    
-# Create a button, that when clicked, shows a text
-if(st.button("START")):
-    st.markdown('##')
-    st.text("Please select the snack that appears to be the most tasty to you.")
-    st.text("If you don't like these snack options, just click start again.")
+    return cluster_product, new_snacks
 
-    # create grid for photos
-    col1, col2 = st.columns(2)
+def make_clickable(link):
+    # target _blank to open new window
+    # extract clickable text to display for your link
+    return f'<a target="_blank" href="{link}">Buy product online</a>'
 
-    col1.header("Product no. 1")
-    col1.write(cl_0_name)
-    col1.image(cl_0_link)
 
-    col2.header("Product no. 2")
-    col2.write(cl_1_name)
-    col2.image(cl_1_link)
-    
-    # create grid for photos
-    col3, col4 = st.columns(2)
-    
-    col3.header("Product no. 3")
-    col3.write(cl_2_name)
-    col3.image(cl_2_link)
+OPTION = st.selectbox("Please enter the name of your favorite snack:", (cl_0_name, cl_1_name, cl_2_name, cl_3_name))
 
-    col4.header("Product no. 4")
-    col4.write(cl_3_name)
-    col4.image(cl_3_link)
+if OPTION:
     
+    st.write('You selected', OPTION)
     
-    st.markdown('##')
-
+    st.markdown('#')
+    st.markdown('#')
     
-    option = st.selectbox("Select your favorite snack from this sample:", ('Choose a snack', cl_0_name, cl_1_name, cl_2_name, cl_3_name))
+    # creating final data frame
+    result = product_recommender_2(df, OPTION)
     
-    if len(option) == 0 or option == 'Choose a snack':
-        st.write("We can understand the decision is difficult :P")
+    # storing chosen cluster in variable
+    cluster = result[0]
     
-    else:
-        #confirming choice
-        st.write('You selected:', option)
+    # products
+    final_snacks = result[1]
+    
+    # creating hyperlinks
+    final_snacks['product_link'] = final_snacks['product_link'].apply(make_clickable)
+    final_snacks = final_snacks.to_html(escape=False)
+    
+    if cluster == 0:
+        st.write("This snack belongs to ***the first cluster***")
+        st.write("These are your product recommendations:")
         
-        # creating final data frame
-        final_snacks = product_recommender(df, option)
+        # create grid for photos
+        st.write(final_snacks, unsafe_allow_html=True)
         
-        # storing chosen cluster in variable
-        cluster = final_snacks[0]
+    if cluster == 1:
+        st.write("***This snack belongs to the second cluster***")
+        st.write("These are your product recommendations:")
+        
+        # create grid for photos
+        st.write(final_snacks, unsafe_allow_html=True)
 
-        if cluster == 0:
-            st.write("This snack belongs to the first cluster")
+    if cluster == 2:
+        st.write("***This snack belongs to the third cluster***")
+        st.write("These are your product recommendations:")
+        
+        # create grid for photos
+        st.write(final_snacks, unsafe_allow_html=True)
 
-        elif cluster == 1:
-            st.write("This snack belongs to the second cluster")
+    if cluster == 3:
+        st.write("***This snack belongs to the forth cluster***")
+        st.write("These are your product recommendations:")
+        
+        # create grid for photos
+        st.write(final_snacks, unsafe_allow_html=True)
+    
+    
 
-        elif cluster == 2:
-            st.write("This snack belongs to the third cluster")
 
-        elif cluster == 3:
-            st.write("This snack belongs to the forth cluster")
+        
+
+        
         
         
